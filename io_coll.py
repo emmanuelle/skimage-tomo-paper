0bc31239bf47710013749f62844030cb5e17f289
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-08-16 17:47:25 +1000

    Prefer functions over object methods

--- io_coll.py
+++ io_coll.py
@@ -1,6 +1,6 @@
 >>> from skimage import io
 
 >>> image_collection = io.imread_collection('*.tif')
->>> image_3d = image_collection.concatenate()
+>>> image_3d = io.concatenate(image_collection)
 >>> print(image_3d.shape)
 (800, 1024, 1024)
